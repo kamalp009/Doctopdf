f0d58a2836b908b5457dbbec85e6879558362453
kamal <sagarme.adepu@gmail.com>
2020-03-04 16:16:55 +0530

    changes in node

--- node_file.js
+++ node_file.js
@@ -1,6 +1,8 @@
 const word2pdf = require('word2pdf');
 const fs = require('fs');
 
+console.log("i am in");
+
 const convert = async () => {
   const data = await word2pdf('demo.docx')
   fs.writeFileSync('demo.pdf', data);
